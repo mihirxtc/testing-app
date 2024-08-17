f84a39578a1694f3b3a3ce240fcc92cfe55a7e8f
mihirxtc <mihirxtc@gmail.com>
2024-08-17 14:47:19 +0000

    testing app version 0.0

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+app.listen(port, () => {
+    console.log(`Server is started on port ${port}`);
+});
